84273a08c596c796734641a091e0b072a1be276c
Kevin English <54373639+Kevin707@users.noreply.github.com>
2021-12-13 22:32:11 +0000

    Q3 done

--- final3.hpp
+++ final3.hpp
@@ -59,3 +59,32 @@ public:
 		cout << expression[i] << "\t";
 		cout << endl;
 	}
+	int Evaluation()
+	{
+		const int N = 20;
+		const int offset = 48;
+		Stack<int, N> stack;
+		char input;
+
+		int opr1, opr2, result;
+
+		for (int i = 0; i < expression.size(); i++)
+		{
+			input = expression[i];
+			if (isdigit(input))
+				stack.push(int(input) - offset);
+			else if (isOperator(input))
+			{
+				opr1 = stack.pop();
+				opr2 = stack.pop();
+				result = calculate(opr1, opr2, input);
+				stack.push(result);
+			}
+		}
+		cout << "The result of the expression is " << stack.pop() << endl;
+
+		return result;
+	}
+};
+
+#endif
