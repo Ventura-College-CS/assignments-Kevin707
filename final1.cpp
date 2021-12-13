31f9b1b2700fbe6fe327cf623c499535ef54d758
Kevin English <54373639+Kevin707@users.noreply.github.com>
2021-12-13 21:59:39 +0000

    Final Q1 done

--- final1.cpp
+++ final1.cpp
@@ -18,3 +18,36 @@ int binarySearch(Course array[], int first, int last, int target)
        return -1;
     
 }
+
+int main()
+{
+        int target;
+        ifstream ifs;
+        ifs.open("class.txt");
+	if ( ifs.fail())
+	{
+		cerr << "File Open Error\n";
+		exit(0);
+	}
+
+        cout << "Which CourseID would you like information for? " << endl;
+        cin >> target;
+    
+        Course coursearray[10];
+        int cid;
+        string cname;
+        int ccredit;
+        int i;
+        for(i=0; i<10; i++)
+        {
+                ifs >> cid >> cname >> ccredit;
+                coursearray[i].setCourse(cid, cname, ccredit);
+        }
+        int x = 10;
+        int result = binarySearch(coursearray, 0, x-1, target);
+        (result == -1)
+                ? cout << "Element is not present in array"
+                : cout << "Element is present at index " << result;
+        return 0;
+    
+}
